586558cdedc8595bb64d2888e0272f6a3fa4b465
vanechka <367191@edu.itmo.ru>
2024-12-21 15:53:13 +0300

    updated

--- lab2_cache/lab2_cache.h
+++ lab2_cache/lab2_cache.h
@@ -3,15 +3,17 @@
 
 #include <cstddef>
 #include <sys/types.h>
+#include <chrono>
+
+typedef std::chrono::high_resolution_clock::time_point access_hint_t;
 
 int lab2_open(const char* path, int flags, mode_t mode);
 int lab2_close(int fd);
-ssize_t lab2_read(int fd, void* buf, size_t count);
-ssize_t lab2_write(int fd, const void* buf, size_t count);
+ssize_t lab2_read(int fd, void* buf, size_t count, access_hint_t hint);
+ssize_t lab2_write(int fd, const void* buf, size_t count, access_hint_t hint);
 off_t lab2_lseek(int fd, off_t offset, int whence);
 int lab2_fsync(int fd_to_sync);
 
-// Stats
 unsigned long lab2_get_cache_hits();
 unsigned long lab2_get_cache_misses();
 void lab2_reset_cache_counters();
